ff3031067ab4f27d569a971984a317eb00df72ca
Dadams11 <adams.1743@osu.edu>
2023-08-14 14:17:05 -0400

    adjusted paths in users.js to make sure redirections are consistent with the /users prefix, which should resolve the navigation problem

--- routes/users.js
+++ routes/users.js
@@ -6,16 +6,19 @@ const User = require('../models/User');
 
 console.log("Users route file loaded");
 
+// Login page
 router.get('/login', (req, res) => {
     console.log("Login GET route hit");
     res.render('login');
 });
 
+// Signup page
 router.get('/signup', (req, res) => {
     console.log("Signup GET route hit");
     res.render('signup');
 });
 
+// Login Route (handle user login POST request)
 router.post('/login', (req, res) => {
     const { username, password } = req.body;
 
@@ -26,29 +29,32 @@ router.post('/login', (req, res) => {
             req.session.user = { id: user.id, username: user.username };
             res.redirect('/users/dashboard');
         } else {
-            res.redirect('/login');
+            res.redirect('/users/login');
         }
     });
 });
 
+// Signup Route (handle user signup POST request)
 router.post('/signup', (req, res) => {
     const { username, password } = req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
 
     User.create(username, hashedPassword, (err) => {
         if (err) throw err;
-        res.redirect('/login');
+        res.redirect('/users/login');
     });
 });
 
+// Logout Route
 router.get('/logout', (req, res) => {
     req.session.user = null;
-    res.redirect('/login');
+    res.redirect('/users/login');
 });
 
+// Dashboard Route
 router.get('/dashboard', (req, res) => {
     if (!req.session.user) {
-        return res.redirect('/login');
+        return res.redirect('/users/login');
     }
     res.render('dashboard', { user: req.session.user });
 });
